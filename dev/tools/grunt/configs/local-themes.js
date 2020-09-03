6268799578fba896425bcaecdc421d60c757d7f6
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2020-09-03 09:47:08 +0200

    Bootstrap CSS configuration.

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Define Themes
+ *
+ * area: area, one of (frontend|adminhtml|doc),
+ * name: theme name in format Vendor/theme-name,
+ * locale: locale,
+ * files: [
+ * 'css/styles-m',
+ * 'css/styles-l'
+ * ],
+ * dsl: dynamic stylesheet language (less|sass)
+ *
+ */
+module.exports = {
+    default_fr: {
+        area: 'frontend',
+        name: 'Magento/luma',
+        locale: 'fr_FR',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+        ],
+        dsl: 'less'
+    },
+    default_en: {
+        area: 'frontend',
+        name: 'Magento/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+        ],
+        dsl: 'less'
+    },
+    backend: {
+        area: 'adminhtml',
+        name: 'Magento/backend',
+        locale: 'en_US',
+        files: [
+            'css/styles-old',
+            'css/styles'
+        ],
+        dsl: 'less'
+    }
+}
